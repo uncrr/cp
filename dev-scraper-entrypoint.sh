8472a3943b6e8a4630b44283855edb9ff413bbc7
uncrr <workubilal@gmail.com>
2025-12-08 10:36:36 +0300

    dev: entrypoint for different full, single runs and loads

---
+++ dev-scraper-entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Run the python scraper once and fetch results to products.json
+cd ./scraper && python3 main.py
---
+++ dev-ui-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Start User interface with live reload
+cd ./ui && npm run dev &
+
+# Keep running
+wait
